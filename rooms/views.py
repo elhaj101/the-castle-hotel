e09fb505564114bc8a21c5810fe9be6e7dc46d71
elhaj101 <alielhajj@outlook.de>
2025-04-06 18:03:21 +0200

    create templates for apps and urls

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from .models import Room
+
+def room_list(request):
+    rooms = Room.objects.filter(is_available=True)
+    return render(request, 'rooms/room_list.html', {'rooms': rooms})
