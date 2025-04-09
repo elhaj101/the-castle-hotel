5f7fd122c7358525eb892dcff08f5f7fb571d39c
elhaj101 <alielhajj@outlook.de>
2025-04-09 10:15:51 +0200

    Update ALLOWED_HOSTS

--- the_castle_hotel/settings.py
+++ the_castle_hotel/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'your-secret-key')  # Use environment variable for security
 DEBUG = os.getenv('DEBUG', 'True') == 'True'  # Default to True, but set to False in production
 ALLOWED_HOSTS = [
-    'the-castle-hotel-d35174139c77.herokuapp.com',  # Your Heroku app name from logs
+    'the-castle-hotel-d35174139c77.herokuapp.com',
     'localhost',
     '127.0.0.1',
 ]
