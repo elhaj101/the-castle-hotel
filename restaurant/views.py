4c37fcbcedd2cf088b07d76761d971327645d93f
elhaj101 <alielhajj@outlook.de>
2025-04-21 15:26:39 +0200

    fix issue in restaurant view

--- restaurant/views.py
+++ restaurant/views.py
@@ -3,9 +3,8 @@ from .models import RestaurantTable
 from comments.models import Comment
 
 def restaurant_list(request):
-    tables = RestaurantTable.objects.filter(is_available=True)
-    comments = Comment.objects.filter(page='restaurant')
-    return render(request, 'restaurant/restaurant_list.html', {'tables': tables, 'comments': comments})
+    tables = RestaurantTable.objects.all()  # Fetch all tables
+    return render(request, 'restaurant/restaurant_list.html', {'tables': tables})
 
 def restaurant_reservation_list(request):
     reservations = RestaurantReservation.objects.all()
