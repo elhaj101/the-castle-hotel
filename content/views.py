b110926a1fe8868cabe0595937d9b47365bb1e10
elhaj101 <alielhajj@outlook.de>
2025-04-23 00:01:24 +0200

    add a contact page

--- content/views.py
+++ content/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'content/home.html')
+
+
+def contact(request):
+    return render(request, 'content/contact.html')
